8f2d4deae8d38dcb93536f89735eb30037d46662
nesbitjd <nesbitjd@gmail.com>
2022-08-09 21:25:18 -0500

    add vela pipeline and update go

--- pkg/commands/play.go
+++ pkg/commands/play.go
@@ -1,27 +1,28 @@
 package commands
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"hangle_cli/pkg/hangman"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/nesbitjd/hangle_server/types"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/xerrors"
 )
 
-type SourceHangman struct {
-	Word     string
-	Failures int
-	Guesses  []string
-}
+var (
+	hangleServerUrl = "http://127.0.0.1:8080"
+)
 
 func play() {
 	// Contact server for word
-	id := 1
-	url := fmt.Sprintf("http://127.0.0.1:8080/api/v1/hangman/%+v", id)
 
-	resp, err := http.Get(url)
+	word_url := "http://127.0.0.1:8080/api/v1/word/last"
+	resp, err := http.Get(word_url)
 	if err != nil {
 		log.Fatalln(err)
 		return
@@ -33,53 +34,99 @@ func play() {
 		return
 	}
 
-	game := &SourceHangman{}
+	word := types.Word{}
 
-	err = json.Unmarshal(body, &game)
+	err = json.Unmarshal(body, &word)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(game)
 
-	h := hangman.Init(game.Word)
+	h := hangman.Init(word.Word)
 
 	// Run game to completion
 	if h.Play() {
 		fmt.Println("You won!")
-		game.uploadResults(h, url)
+	} else {
+		fmt.Println("You suck.")
+	}
+
+	// Retrieve user and validate input
+
+	var username string
+	for !func(u string) bool {
+		valid := true
+		fmt.Println("Enter name to upload results:")
+		fmt.Scanln(&u)
+		username = u
+
+		if u == "" {
+			fmt.Println("Invalid input: empty string")
+			valid = false
+		}
+
+		return valid
+	}(username) {
+	}
+
+	u, err := checkUser(username, hangleServerUrl)
+	if err != nil {
+		log.Fatalln(err)
 		return
 	}
 
-	fmt.Println("You suck.")
-	game.uploadResults(h, url)
-	// Upload results to server
+	if u.Username == "" {
+		u.Username = username
 
+		u.PostUser(hangleServerUrl)
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+	}
+
+	record := types.Record{
+		Word:     word,
+		User:     *u,
+		Failures: h.Failures,
+		Guesses:  strings.Join(h.Guesses, ","),
+	}
+
+	// Upload results to server
+	record.PostResults(hangleServerUrl)
 }
 
-func (game *SourceHangman) uploadResults(h *hangman.Hangman, url string) error {
-	game.Failures = h.Failures
-	game.Guesses = h.Guesses
-	fmt.Println(*game)
+// checkUser takes a lookupUser to look up, and the base_url for the api.
+// If found, returns a pointer to the user. Otherwise returns empty user
+func checkUser(lookupUser string, base_url string) (*types.User, error) {
+	user_url := "http://127.0.0.1:8080/api/v1/user"
 
-	putBody, err := json.Marshal(game)
+	resp, err := http.Get(user_url)
 	if err != nil {
-		return err
+		return nil, xerrors.Errorf("unable to lookup user: %w", err)
 	}
 
-	client := &http.Client{}
-
-	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(putBody))
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, xerrors.Errorf("unable to read user body: %w", err)
 	}
 
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	resp, err := client.Do(req)
+	users := []types.User{}
+
+	err = json.Unmarshal(body, &users)
 	if err != nil {
-		return err
+		return nil, xerrors.Errorf("unable to unmarshal user body: %w", err)
 	}
 
-	fmt.Println(resp.StatusCode)
-	return nil
+	for _, u := range users {
+		if u.Username == lookupUser {
+			logrus.Infof("user %q found", lookupUser)
+			return &u, nil
+		}
+	}
+
+	logrus.Infof("user %q not found", lookupUser)
+	u := &types.User{}
+	return u, nil
+
 }
